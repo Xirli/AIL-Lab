41fdfbb306752295e2086354c0022caef7ed74a1
xirli <47503746+Xirli@users.noreply.github.com>
2021-11-01 23:50:27 +0200

    reformat main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,5 @@
 from src.feedforward import FeedForward
 
-
-
-
-
 from PIL import Image
 from numpy import asarray
 
@@ -14,38 +10,12 @@ image = Image.open(train_path + "sdfergthyl.jpg")
 data = asarray(image)
 print(data)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 dataset = [
     {"in": [0, 0], "out": [0]},
     {"in": [0, 1], "out": [0]},
     {"in": [1, 0], "out": [0]},
     {"in": [1, 1], "out": [1]}
 ]
-
 
 print("AND")
 neural = FeedForward(2, 3, 1)
